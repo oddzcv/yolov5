9f849f52c61f5b724eea9443e719501d1bc0ee77
oddzcv <audyfbrynti@gmail.com>
2025-09-02 13:27:08 +0800

    Update val.py
    normalize = False
    
    Signed-off-by: oddzcv <audyfbrynti@gmail.com>

--- val.py
+++ val.py
@@ -425,7 +425,7 @@ def run(
 
     # Plots
     if plots:
-        confusion_matrix.plot(save_dir=save_dir, names=list(names.values()))
+        confusion_matrix.plot(save_dir=save_dir, names=list(names.values()), normalize=False)
         callbacks.run("on_val_end", nt, tp, fp, p, r, f1, ap, ap50, ap_class, confusion_matrix)
 
     # Save JSON
