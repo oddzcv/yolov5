65dc4ebcd9027d07a9ae87359398bd97a06466d0
oddzcv <audyfbrynti@gmail.com>
2026-02-23 20:13:15 +0800

    Update params for detect.py
    Signed-off-by: oddzcv <audyfbrynti@gmail.com>

--- detect.py
+++ detect.py
@@ -72,8 +72,8 @@ def run(
     source=ROOT / "data/images",  # file/dir/URL/glob/screen/0(webcam)
     data=ROOT / "data/coco128.yaml",  # dataset.yaml path
     imgsz=(640, 640),  # inference size (height, width)
-    conf_thres=0.25,  # confidence threshold
-    iou_thres=0.45,  # NMS IOU threshold
+    conf_thres=0.35,  # confidence threshold
+    iou_thres=0.1,  # NMS IOU threshold
     max_det=1000,  # maximum detections per image
     device="",  # cuda device, i.e. 0 or 0,1,2,3 or cpu
     view_img=False,  # show results
@@ -94,7 +94,7 @@ def run(
     line_thickness=3,  # bounding box thickness (pixels)
     hide_labels=False,  # hide labels
     hide_conf=False,  # hide confidences
-    half=False,  # use FP16 half-precision inference
+    half=True,  # use FP16 half-precision inference
     dnn=False,  # use OpenCV DNN for ONNX inference
     vid_stride=1,  # video frame-rate stride
 ):
